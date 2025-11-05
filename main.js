e70040c02af25217437be80209e1af0f1f091df7
dmoyamur <dmoya844@gmail.com>
2025-11-05 12:48:11 -0500

    Ejercicios 1 y 2

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-import { PI, IVA } from "./constantes.js";
+import { PI, IVA, DIAS_SEMANA } from "./constantes.js";
+import { sumar, restar, multiplicar, areaCirculo1} from "./operaciones-matematicas.js";
 
+console.log(" EJERCICIO 1")
 const areaCirculo = PI*5*5;
 console.log(`El área del círculo es: ${areaCirculo}`);
+
+const calculoiva = IVA*100;
+const totaliva = calculoiva+100
+console.log(`El precio con IVA es de: ${totaliva}`)
+
+console.log(`El tercer día de la semana es: `,DIAS_SEMANA[2])
+
+console.log("\n EJERCICIO 2")
+const suma = console.log(`La suma de los números es: `,sumar(5,4));
+const resta = console.log(`La resta de los números es: `,restar(5,4));
+const multiplicacion = console.log(`La multiplicación de los números es: `,multiplicar(5,4));
+const areaCirc = console.log(`El área del círculo es: `,areaCirculo1(8));
---
+++ operaciones-matematicas.js
@@ -0,0 +1,17 @@
+import {PI, IVA} from "./constantes.js"
+
+export function sumar(a,b){
+    return (a+b);
+};
+
+export function restar(a,b){
+    return (a-b);
+};
+
+export function multiplicar(a,b){
+    return (a*b);
+};
+
+export function areaCirculo1(radio){
+    return(PI*radio*radio)
+}
